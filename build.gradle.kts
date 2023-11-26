df04a934c119764b5e15de54d09c171de63a2686
karthi.tamil <karthi.tamil@mercari.com>
2023-11-26 14:47:25 +0900

    initial commit -Db setup (SqlDelight) -DI setup (Koin) -Architecture (Decompose)

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    //trick: for the same plugin versions in all sub-modules
+    id("com.android.application").version("8.1.1").apply(false)
+    id("com.android.library").version("8.1.1").apply(false)
+    kotlin("android").version("1.8.21").apply(false)
+    kotlin("multiplatform").version("1.8.21").apply(false)
+}
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
+        classpath(Dep.SqlDelight.gradlePlugin)
+        classpath("com.android.tools.build:gradle:8.1.2")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "pathivu"
+include(":androidApp")
+include(":dashboard")
+include(":domain_words")
